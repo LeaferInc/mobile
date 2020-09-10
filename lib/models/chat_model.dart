e1421695cc01c8d90e32793c3af9c6c0e7efa433
Tilkeii <florian@gouarin.info>
2020-09-10 23:38:49 +0200

    connexion au socket

--- lib/models/chat_model.dart
+++ lib/models/chat_model.dart
@@ -13,5 +13,12 @@ class ChatModel{
   void init() async{
     currentUser = await UserService.getCurrentUser();
     socketIO = await SocketioService.initSocket();
+    socketIO.on('init', (data) => print("[init]" + data.toString()));
+    socketIO.on('error', (data) => print("[error] " + data.toString()));
+    socketIO.on('disconnect', (data) => print("[disconnect] " + data.toString()));
+    socketIO.on('connecting', (data) => print("[connecting] " + data.toString()));
+    socketIO.on('connect', (data) => print("[connect] " + data.toString()));
+    socketIO.on('connect_error', (data) => print("[connect_error] " + data.toString()));
+    socketIO.on('connect_timeout', (data) => print("[connect_timeout] " + data.toString()));
   }
 }
--- lib/services/socketio_service.dart
+++ lib/services/socketio_service.dart
@@ -3,30 +3,19 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:socket_io_client/socket_io_client.dart';
 
 class SocketioService {
-  static const _BASE_URL_SOCKET = RestDatasource.HOST + "/chat";
+  // static const _BASE_URL_SOCKET = RestDatasource.HOST + "/chat";
 
   static Future<Socket> initSocket() async {
-    Socket socketIO;
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    String token = await prefs.getString('jwt');
-    socketIO = io(_BASE_URL_SOCKET, <String, dynamic>
+    String token = prefs.getString('jwt');
+    Socket socketIO = io("http://10.0.2.2:3000/chat", <String, dynamic>
       {
         'transports': ['websocket'],
-        'transportOptions': {
-          'polling': {
-            'extraHeaders': {
-              'Authorization': 'Bearer ' + token
-            }
-          }
-        },
-        'path': '/chat',
-        'autoConnect': false,
+        'extraHeaders': {
+          'Authorization': 'Bearer ' + token
+        }
       }
     );
-    socketIO.on('connect', (_) {
-      print('connect');
-    });
-    socketIO.connect();
     return socketIO;
   }
 }
